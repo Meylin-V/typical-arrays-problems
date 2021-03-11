16d14d5d2bc9c6ab2374c273c507c94a5f5f3dca
Meylin-V <maksymenko.nataliia@gmail.com>
2021-03-11 21:23:12 +0200

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  let mint = Infinity;
+  if (typeof array === 'undefined' || array.length == 0) {return 0}
+  else {
+  for (let i = 0; i < array.length; i++) {
+      if (array[i] < mint) {
+          mint = array[i]
+      }
+  }
+  return mint;}
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxt = -Infinity;
+  if (typeof array === 'undefined' || array.length == 0) {
+      return 0}
+  else {
+  for (let i = 0; i < array.length; i++) {
+      if (array[i] > maxt) {
+          maxt = array[i]
+      }
+  }
+  return maxt;}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  if (typeof array === 'undefined' || array.length == 0) {return 0}
+  else {
+  for (let i = 0; i < array.length; i++) {
+      sum = sum + array[i];
+      }
+  let avgt = sum / array.length
+  return avgt;}
 }
